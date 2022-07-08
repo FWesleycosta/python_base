f597bdfa28dd9e99a5a1251df266fd0ae5d63a6b
Francisco Wesley <francisco.costa@tivit.com>
2022-07-08 17:58:39 -0300

    Add project multiplication table

--- tabuada.py
+++ tabuada.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3 
 """ Print the multiplication table from 1 to 10
 
-Multiplication table of 1:
+---Multiplication table of 1---
 
-1 x 1 
-1 x 2 
-1 x 3
-------------
-Multiplication table of 2:
+    1 x 1 = 1
+    1 x 2 = 2
+    1 x 3 = 3
+...
+###############
+---Multiplication table of 2---
 
-2 x 1 
-2 x 2 
-2 x 3
+    2 x 1 = 2 
+    2 x 2 = 4
+    2 x 3 = 6
+...
+#############
 """
 __version__ = "0.1.0"
 __author__ = "Francisco Wesley"
 __license__= "Unlicense"
 
-#base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-numeros = list(range (1,11))
+numeros = list(range (1,21))
 
 # Para cada numero em numeros:
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("------------")
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 18)
