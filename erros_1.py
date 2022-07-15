d70d5f8586187ff9bf941e2f5b724a7deadd2140
Francisco Wesley <francisco.costa@tivit.com>
2022-07-14 21:26:45 -0300

    Tips: Errors EAFP

---
+++ erros_1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from logging import exception
+import os
+import sys
+
+# Easy to ASK Forgiveness than permission
+
+
+try:
+    names = open("names.txt").readlines() #FileNotFoundError
+    1/1 #ZeroDivisionError
+    print(names.append)
+except FileNotFoundError as error:
+    print(f"{str(error)}.")
+    sys.exit(1)
+except ZeroDivisionError:
+    print("[Error] You cant divide by zero!!")
+    sys.exit(1)
+else: #Execute in cause error
+    print("Sucesso!!!")
+finally: # Forever execution
+    print("Execute isso sempre!")
+    
+try:
+    print(names[2])
+except:
+    print("[Error] Missing name in the list")
+    sys.exit(1)
