bd442377a95d129392a694245d91ab093883cba9
Francisco Wesley <francisco.costa@tivit.com>
2022-07-15 09:22:40 -0300

    Update archived log

--- logs.py
+++ logs.py
@@ -4,24 +4,31 @@ __version__= "0.1.0"
 __author__= "Francisco Wesley"
 
 import logging
+import os
+from logging import handlers
 
 # my instance
+
+log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
+
 log = logging.Logger("log.py", logging.DEBUG)
 
+
 #level / create console handler
-ch = logging.StreamHandler()  #Console/terminal/stderr
-ch.setLevel(logging.DEBUG)
+#ch = logging.StreamHandler()  #Console/terminal/stderr
+#ch.setLevel(logging.DEBUG)
+
+fh = handlers.RotatingFileHandler("meulog.log", maxBytes=10**6, backupCount=10)
+fh.setLevel(log_level)
 
 #formatting
 fmt = logging.Formatter(
     '%(asctime)s | archive: %(name)s | %(levelname)s | '
     'line: %(lineno)d | message: %(message)s'
 )
-ch.setFormatter(fmt)
-
+fh.setFormatter(fmt)
 #send log for console handler
-log.addHandler(ch)
-
+log.addHandler(fh)
 
 '''
 log.debug("Mensagem para o time técnico")
